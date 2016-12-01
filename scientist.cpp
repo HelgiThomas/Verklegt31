a9f3fe284b5c2e5c88d28a004039f573d27e8e49
sigrunelva <sigrunr16@ru.is>
2016-12-01 13:21:12 +0000

    Commenting scientist.cpp

--- scientist.cpp
+++ scientist.cpp
@@ -2,11 +2,22 @@
 
 using namespace std;
 
+/*
+ * This is the default constructor.
+ */
 Scientist::Scientist()
 {
 
 }
 
+/*
+ * This function is a constructor that sets the information about
+ * each scientist.
+ * @param: string name, sets the name.
+ * @param: string sex, sets the sex.
+ * @param: int birth, sets the birth year.
+ * @param: int death, sets the death year.
+ */
 Scientist::Scientist(string name,string sex, int birth, int death)
 {
     _name = name;
@@ -16,21 +27,37 @@ Scientist::Scientist(string name,string sex, int birth, int death)
 
 }
 
+/*
+ * This function returns the name.
+ * @return: string name.
+ */
 string Scientist::getName () const
 {
     return _name;
 }
 
+/*
+ * This function returns the sex.
+ * @return: string sex.
+ */
 string Scientist::getSex () const
 {
     return _sex;
 }
 
+/*
+ * This function returns the birth year.
+ * @return: int birth.
+ */
 int Scientist::getBirth () const
 {
     return _birth;
 }
 
+/*
+ * This function returns the death year.
+ * @return: int death.
+ */
 int Scientist:: getDeath () const
 {
     return _death;
