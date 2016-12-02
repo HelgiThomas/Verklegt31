e88926b2697372a5b2b80ee8b7f0d53f057b2e4f
sigrunelva <sigrunr16@ru.is>
2016-12-02 14:26:52 +0000

    Laga comment- scientist

--- model/scientist.cpp
+++ model/scientist.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-/*
- * This is the default constructor.
+/**
+ * @brief This is the default constructor.
  */
 Scientist::Scientist()
 {
@@ -14,13 +14,13 @@ Scientist::Scientist()
     _citation = "";
 }
 
-/*
- * This function is a constructor that sets the information about
+/**
+ * @brief This function is a constructor that sets the information about
  * each scientist.
- * @param: string name, sets the name.
- * @param: string sex, sets the sex.
- * @param: int birth, sets the birth year.
- * @param: int death, sets the death year.
+ * @param string name, sets the name.
+ * @param string sex, sets the sex.
+ * @param int birth, sets the birth year.
+ * @param int death, sets the death year.
  */
 Scientist::Scientist(string name,string sex, int birth, int death , string citation)
 {
@@ -29,93 +29,92 @@ Scientist::Scientist(string name,string sex, int birth, int death , string citat
     _birth = birth;
     _death = death;
     _citation = citation;
-
 }
 
-/*
- * This function returns the name.
- * @return: string name.
+/**
+ * @brief This function returns the name.
+ * @return string name.
  */
 string Scientist::getName () const
 {
     return _name;
 }
 
-/*
- * This function returns the sex.
- * @return: string sex.
+/**
+ * @brief This function returns the sex.
+ * @return string sex.
  */
 string Scientist::getSex () const
 {
     return _sex;
 }
 
-/*
- * This function returns the birth year.
- * @return: int birth.
+/**
+ * @brief This function returns the birth year.
+ * @return int birth.
  */
 int Scientist::getBirth () const
 {
     return _birth;
 }
 
-/*
- * This function returns the death year.
- * @return: int death.
+/**
+ * @brief This function returns the death year.
+ * @return int death.
  */
 int Scientist:: getDeath () const
 {
     return _death;
 }
 
-/*
- * This function returns the citation.
- * @return: string citation.
+/**
+ * @brief This function returns the citation.
+ * @return string citation.
  */
 string Scientist:: getCitation() const
 {
     return _citation;
 }
 
-/*
- * This function sets the name.
- * @param: string name.
+/**
+ * @brief This function sets the name.
+ * @param string name.
  */
 void Scientist::setName (string name)
 {
     _name = name;
 }
 
-/*
- * This function sets the sex.
- * @param: string sex.
+/**
+ * @brief This function sets the sex.
+ * @param string sex.
  */
 void Scientist::setSex (string sex)
 {
     _sex = sex;
 }
 
-/*
- * This function sets the citation.
- * @param: string citation.
+/**
+ * @brief This function sets the citation.
+ * @param string citation.
  */
 void Scientist::setCitation(string citation)
 {
     _citation = citation;
 }
 
-/*
- * This function sets the birth year.
- * @param: int birth.
+/**
+ * @brief This function sets the birth year.
+ * @param int birth.
  */
 void Scientist::setBirth (int birth)
 {
     _birth = birth;
 }
 
-/*
- * This function sets the death year.
- * @param: int death.
+/**
+ * @brief This function sets the death year.
+ * @param int death.
  */
 void Scientist::setDeath (int death)
 {
