3078cd87cbdcb7151baf14d0ed1813af6a18c7ac
Emil Hjaltason <emilhjaltason@Emils-MacBook-Pro.local>
2016-12-07 14:08:31 +0000

    last fixes vol2

--- data/scientistaccess.cpp
+++ data/scientistaccess.cpp
@@ -24,7 +24,7 @@ void ScientistAccess::readToDatabase(Scientist scientist)
         QString qDeath = QString::number(scientist.getDeath());
         QString qStatus = QString::number(number);
 
-        query.prepare("INSERT INTO Computers (id, name, sex, birth, death, status) VALUES (:id, :name, :sex, :birth, :death, :status)");
+        query.prepare("INSERT INTO Scientists (id, name, sex, birth, death, status) VALUES (:id, :name, :sex, :birth, :death, :status)");
         query.bindValue(":id", qId);
         query.bindValue(":name", qName);
         query.bindValue(":sex", qSex);
